724fb35f7ba45edda3728995d5e2918af15be927
Aldea <mihai.aldea@atos.net>
2018-12-10 10:28:45 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require ('body-parser');
+var {ObjectID} = require ('mongodb');
 
 var {mongoose} = require ('./db/mongose')
 var {Todo} = require('./models/todo');
@@ -29,6 +30,36 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+//GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+    //valid id using isValid
+      //404-- send back empty value
+
+    //findById
+        //success
+            //if todo - send it back
+            //if no todo--send back 404 with empty body
+        //error
+          //400 - 
+});
+
+
 
 app.listen(3000, () => {
     console.log('Server started at port 3000');
