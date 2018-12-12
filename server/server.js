d6b3ba0263fba4eb9389fbdc3cb44d158eeb392d
Aldea <mihai.aldea@atos.net>
2018-12-12 11:52:42 +0200

    Add POST /users/login/route

--- server/server.js
+++ server/server.js
@@ -112,9 +112,6 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-
-
-
     user.save().then(()=> {
        return  user.generateAuthToken();
         // res.send(user);
@@ -125,11 +122,22 @@ app.post('/users', (req, res) => {
     })
 })
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
+
+// POST /users/login {email, password}
+app.post('/users/login',(req, res) => {
+    var body = _.pick(req.body, ['email','password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token)=> {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
